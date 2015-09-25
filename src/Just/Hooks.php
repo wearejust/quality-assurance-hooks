8b0fdeb3a3a92e01c2cfa9c281bcdafa1a1a6556
Cees van Egmond <cees.vanegmond@wearejust.com>
2015-09-25 16:01:19 +0200

    Feature: Hooks - Added rights path for hook - Added default cs fixer

--- src/Just/Hooks.php
+++ src/Just/Hooks.php
@@ -9,15 +9,19 @@ class Hooks
     public static function checkHooks(Event $event)
     {
         $io = $event->getIO();
-        $gitHook = @file_get_contents(__DIR__.'/../.git/hooks/pre-commit');
-        $docHook = @file_get_contents(__DIR__.'/../bin/pre-commit');
+        $newPath = __DIR__.'/../../../../../.git/hooks/pre-commit';
+
+        $gitHook = @file_get_contents($newPath);
+        $docHook = @file_get_contents(__DIR__.'/../../../../../bin/pre-commit');
 
         if ($gitHook !== $docHook) {
             $io->write('<error>GIT hooks ontbreken</error>');
                 exec(
                     'cd .git/hooks && '.
-                    'ln -s ../bin/pre-commit ./hooks/pre-commit'
+                    'ln -s ../../bin/pre-commit ./pre-commit'
                 );
+                copy(__DIR__ . '/../../.php_cs', __DIR__.'/../../../../../.php_cs');
+                chmod($newPath, 0755);
         }
     }
 }
