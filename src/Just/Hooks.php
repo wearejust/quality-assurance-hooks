dd30a4f07d36402498970199504500e53a89ed3c
John in 't Hout <john.inthout@wearejust.com>
2015-11-19 10:28:22 +0100

    Hotfix: Also check if there is an laravel folder to symlink to since this else the symlink will fill to execute.

--- src/Just/Hooks.php
+++ src/Just/Hooks.php
@@ -16,7 +16,15 @@ class Hooks
         $newPath = __DIR__.'/../../../../../.git/hooks/pre-commit';
 
         $gitHook = @file_get_contents($newPath);
-        $docHook = @file_get_contents(__DIR__.'/../../../../../bin/pre-commit');
+        
+        $docHookPath = __DIR__.'/../../../../../';
+        $docHookFile = $docHookPath . 'bin/pre-commit';
+        
+        if(!file_exists($docHookPath . 'bin/pre-commit')){
+            $docHookFile = $docHookPath . 'laravel/bin/pre-commit';
+        }
+        
+        $docHook = @file_get_contents($docHookFile);
 
         if ($gitHook !== $docHook) {
             $io->write('<error>GIT hooks ontbreken</error>');
